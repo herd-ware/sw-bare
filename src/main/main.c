96a781e965fa630aa70fe25ec4ac07555c6ab529
Mathieu Escouteloup <mathieu.escouteloup@gmail.com>
2023-04-03 12:25:01 +0200

    Displays boot message via serial port

--- src/main/main.c
+++ src/main/main.c
@@ -3,7 +3,7 @@
  * Created Date: 2023-03-31 02:58:56 pm
  * Author: Mathieu Escouteloup
  * -----
- * Last Modified: 2023-03-31 03:40:36 pm
+ * Last Modified: 2023-04-03 10:34:27 am
  * Modified By: Mathieu Escouteloup
  * -----
  * License: See LICENSE.md
@@ -19,5 +19,12 @@
 uint32_t aaa = 0xaabbccdd;
 
 void main() {  
-  
+  gpio_set_bit(GPIOA, 10);
+  uart_config(UART0, 1, 1, 1, 1, 0, UART_BAUD_230400);
+
+  uart_send_array(UART0, "\nStart bare-metal software ...\n", 31);
+  uart_send_array(UART0, "[OK] Initialization.\n", 21);
+  while(!uart_status_idle(UART0));
+
+  //wait_cycle(5000);
 }
